f431de2ea1b35e8419a7347bb1264ad63e43e3ee
Stephane Bellity <sbellity@gmail.com>
2014-05-20 14:24:00 +0200

    Better friends selector

--- src/friends-selector/main.js
+++ src/friends-selector/main.js
@@ -1,39 +1,184 @@
 Hull.component({
   templates: ['main'],
-  require: ['https://connect.facebook.net/en_US/all.js'],
 
   datasources: {
     friends: function() {
       if (this.loggedIn()) {
-        return this.api({ provider: 'facebook', path: 'me/friends' }, { fields: 'id,name,picture' });
+        return this.api({ provider: 'facebook', path: 'me/friends' }, { fields: this.friendsFields(), limit: 1000 });
       }
     }
+  },
+
+  initialize: function() {
+    this.method = (this.options.method || 'share').toLowerCase();
+    this.buildParams = this.buildParamsFor[this.method] || function(params) { return params; };
   },
 
   events: {
     'submit form': function(e) {
       e.preventDefault();
-      var data = this.sandbox.dom.getFormData(this.$(e.target));
-      var self = this, _ = this.sandbox.util._;
-      var params = { message: data.message }
-      if (data.friends && data.friends.length > 0) {
-        params.to = data.friends.join(',');
+      var self = this;
+      var formData = this.sandbox.dom.getFormData(this.$(e.target));
+      var params = this.buildParams(formData);
+      this.api({ provider: 'facebook', path: 'ui.' + this.method }, params).then(function(response) {
+        self.afterSent.call(self);
+      });
+    },
+
+    'change input[type="checkbox"]': function(e) {
+      if (e.target && e.target.type === 'checkbox') {
+        var $friend = this.$("[data-friend='" + $(e.target).val() + "']");
+        if (e.target.checked) {
+          $friend.addClass('bg-info');
+        } else {
+          $friend.removeClass('bg-info');
+        }
+        var val, countFriends = this.$('input[type="checkbox"]:checked').length;
+        if (countFriends > 0) {
+          val = "Send to " + this.$('input[type="checkbox"]:checked').length + " friends";
+        } else {
+          val = "Send";
+        }
+        this.$sendBtn.val(val);
       }
+    }
+  },
+
+  actions: {
+    // TODO: make this more efficient
+    selectAll: function(event, action) {
+      this.$('input[type="checkbox"]').each(function(i,c) {
+        if (!c.checked) {
+          $(c).click();
+        }
+      });
+    },
+    unselectAll: function(event, action) {
+      this.$('input[type="checkbox"]').each(function(i,c) {
+        if (c.checked) {
+          $(c).click();
+        }
+      });
+    }
+  },
+
+  buildParamsFor: {
+    
+    share: function(params) {
+      params.href = params.href || this.options.href || document.location.toString();
+      if (params.friends && params.friends.length > 0) {
+        params.to = params.friends.join(',');
+        delete params.friends;
+      }
+      return params;
+    },
+
+    apprequests: function(params) {
+      if (params.friends && params.friends.length > 0) {
+        params.to = params.friends.join(',');
+        delete params.friends;
+      }
+
       if (this.options.redirectUri) {
         params.redirect_uri = this.options.redirectUri;
       }
 
       if (this.options.title) {
-        params.title = this.options.title;
+        params.title = this.options.title || "Come play with me";
       }
 
-      self.api({ provider: 'facebook', path: 'ui.apprequests' }, params).then(function(response) {
-        console.warn("YEAH : ", response)
-      });
+      return params;
+    }
+  },
+
+  helpers: {
+    grid: function(list, columns, options) {
+      var row_start = "<div class='row'>";
+      var row_end = "</div>";
+      var ret = row_start;
+      var colClass = 'col-md-' + 12 / columns;
+      for (var i=0, j=list.length; i<j; i++) {
+        ret += "<div class='" + colClass + "'>" + options.fn(list[i]) + "</div>";
+        if (i > 0 && ((i+1) % columns == 0)) {
+          ret += row_end;
+          if ((i+1) < j) {
+            ret += row_start;
+          }
+        }
+      }
+      if (((list.length) % columns) != 0) {
+        ret += row_end;
+      }
+      return ret;
     }
   },
 
   beforeRender: function(data) {
-    
+    data.friendsList = this.filterFriends(data.friends.data || []);
+  },
+
+  afterRender: function() {
+    var $grid = this.$('.users-grid');
+    $grid.css({
+      overflowY: 'auto',
+      overflowX: 'hidden',
+      maxHeight: '300px'
+    });
+    $grid.removeClass('hidden');
+    this.$sendBtn = this.$('input[type="submit"]');
+  },
+
+  afterSent: function() {
+    var $btn = this.$sendBtn;
+    this.actions.unselectAll.call(this);
+    $btn.val("Sent !");
+    $btn.attr('disabled', true);
+    setTimeout(function() {
+      $btn.val("Send");
+      $btn.attr('disabled', false);
+    }, 1000);
+  },
+
+  friendsFields: function() {
+    var _ = this.sandbox.util._;
+    var fields = (this.options.friendsFields || '').split(',') || [];
+    var filters = this.friendsFilters();
+    _.each(filters, function(filter) {
+      if (_.isObject(filter)) {
+        fields = fields.concat(_.keys(filter));  
+      }
+    });
+    fields = fields.concat(['id','name','picture']);
+    return  _.compact(_.uniq(fields));
+  },
+
+  friendsFilters: function() {
+    var _ = this.sandbox.util._, friendsFilters;
+    if (!_.isArray(this.options.friendsFilters)) {
+      friendsFilters = _.compact([this.options.friendsFilters]);
+    } else {
+      friendsFilters = this.options.friendsFilters;
+    }
+    return friendsFilters;
+  },
+
+  filterFriends: function(friends) {
+    var ret, _ = this.sandbox.util._, friendsFilters = this.friendsFilters();
+
+    if (friendsFilters.length > 0) {
+      ret = friends;
+    } else {
+      ret = _.filter(friends, function(friend) {
+        var ret = [];
+        _.each(friendsFilters, function(filter) {
+          if (_.isObject(filter)) {
+            var filterKeys = _.keys(filter);
+            ret.push(_.isEqual(_.pick(friend, filterKeys), filter));
+          }
+        });
+        return _.any(ret);
+      });
+    }
+    return _.sortBy(ret, function(f) { return f.name });
   }
 });
